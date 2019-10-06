79502fa20ef2eced984db965c694d172f26123ff
unknown <huzaifdharejo@gmail.com>
2019-10-06 15:52:59 -0700

    email added to user

--- DatingApp/Models/User.cs
+++ DatingApp/Models/User.cs
@@ -10,6 +10,8 @@ namespace DatingApp.Models
         public int Id { get; set; }
         public string Name { get; set; }
 
+        public string Email { get; set; }
+
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
 
