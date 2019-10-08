16a1e027b44abd27d858cc41057fc6be71a5ca67
unknown <huzaifdharejo@gmail.com>
2019-10-08 15:52:26 -0700

    Done Error Heanddling

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,31 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../_services/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../_services/auth.service";
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  selector: "app-nav",
+  templateUrl: "./nav.component.html",
+  styleUrls: ["./nav.component.css"]
 })
 export class NavComponent implements OnInit {
-model: any = {};
-  constructor(private authServices: AuthService) { }
+  model: any = {};
+  constructor(private authServices: AuthService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  login() {
+    this.authServices.login(this.model).subscribe(
+      next => {
+        console.log("Logged In Successfully");
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
-login() {
-          this.authServices.login(this.model).subscribe(next => {
-          console.log('Logged In Successfully');}
-          , error => {
-          console.log('Failed to Login');
- }
-  ) ;
-}
-loggedIn(){
-  const token = localStorage.getItem('token');
-  return !!token;
-}
-logOut(){
-  localStorage.removeItem('token');
-  console.log('Logged Out');
-}
+  loggedIn() {
+    const token = localStorage.getItem("token");
+    return !!token;
+  }
+  logOut() {
+    localStorage.removeItem("token");
+    console.log("Logged Out");
+  }
 }
--- DatingApp/Startup.cs
+++ DatingApp/Startup.cs
@@ -14,6 +14,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using DatingApp.Helpers;
 
 namespace DatingApp
 {
@@ -59,7 +63,19 @@ namespace DatingApp
             }
             
             app.UseRouting();
-            
+            app.UseExceptionHandler(builder =>
+            {
+                builder.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    var error = context.Features.Get<IExceptionHandlerFeature>();
+                    if (error != null)
+                    {
+                        context.Response.AddApplicationError(error.Error.Message);
+                        await context.Response.WriteAsync(error.Error.Message);
+                    }
+                });
+            });
             app.UseCors("EnableCORS");
             
             app.UseAuthorization();
