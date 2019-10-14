461394f6bda17a042fbf6268700229a888debaee
Huzaif <huzaifDharejo@gmail.com>
2019-10-14 09:26:45 -0700

    created Good looking views for user detail page

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -5,6 +5,9 @@ import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guardes/auth.guard';
 import { ContentChildren } from '@angular/core';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolver/member-detail.resolver';
+import { MemberListResolver } from './_resolver/member-list.resolver';
 
 export const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -13,7 +16,16 @@ export const appRoutes: Routes = [
     runGuardsAndResolvers: 'always',
     canActivate: [AuthGuard],
     children: [
-      { path: 'member', component: MemberListComponent },
+      {
+        path: 'member',
+        component: MemberListComponent,
+        resolve: { user: MemberListResolver }
+      },
+      {
+        path: 'member/:id',
+        component: MemberDetailComponent,
+        resolve: { user: MemberDetailResolver }
+      },
       { path: 'messages', component: MessagesComponent },
       { path: 'lists', component: ListsComponent }
     ]
