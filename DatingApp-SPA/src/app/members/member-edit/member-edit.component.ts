ac37cbd1f72fae0b65bac5127bdf49b966c94307
Huzaif <huzaifDharejo@gmail.com>
2019-10-16 17:05:09 -0700

    fixed some bugs in User edit commponent

--- DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from 'src/app/_models/User';
 import { ActivatedRoute } from '@angular/router';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-member-edit',
@@ -8,13 +10,20 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
+ editForm: NgForm;
   user: User;
 
-  constructor(private router: ActivatedRoute) {}
+  constructor(private router: ActivatedRoute, private alertify: AlertifyService) {}
 
   ngOnInit() {
     this.router.data.subscribe(data => {
       this.user = data.user;
     });
+  }
+  updateUser() {
+    console.log(this.user);
+    this.alertify.success('Data saved');
+    this.editForm.reset(this.user);
+
   }
 }
